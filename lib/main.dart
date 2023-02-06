e96df1524d51b3301bc64be80a484cc9c772e354
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2023-02-06 22:20:53 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+/*
+x= r * cos(a)
+y= r * sin(a)
+
+angle in flutter clockwise
+pi = 180
+***************************************
+
+0.0 => 0 degrees
+0.5 => 180 degrees
+1.0 => 360 degrees
+
+*/
+import 'dart:math' show pi;
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App Title',
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 2 * pi,
+    ).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()..rotateY(_animation.value),
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5.0,
+                          blurRadius: 7.0,
+                          offset: const Offset(0, 3))
+                    ]),
+              ),
+            );
+          },
+          animation: _animation,
+        ),
+      ),
+    );
+  }
+}
